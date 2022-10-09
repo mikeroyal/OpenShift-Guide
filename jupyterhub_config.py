12cfb44783d7c4494509e91b1c935d3d6b0fb77a
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2022-10-09 00:24:04 -0700

    Added jupyterhub_config.

---
+++ jupyterhub_config.py
@@ -0,0 +1,20 @@
+# This is a Work in Progress! 
+
+import os
+
+import wrapt
+
+from kubernetes.client.configuration import Configuration
+from kubernetes.config.incluster_config import load_incluster_config
+from kubernetes.client.api_client import ApiClient
+from kubernetes.client.rest import ApiException
+from openshift.dynamic import DynamicClient
+
+
+load_incluster_config()
+
+import urllib3
+urllib3.disable_warnings()
+instance = Configuration()
+instance.verify_ssl = False
+Configuration.set_default(instance)
