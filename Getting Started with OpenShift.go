2fba7a76afdb616d8de67089dbc06388a5648ef0
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2022-10-11 00:40:41 -0700

    Updated file.

--- Getting Started with OpenShift.go
+++ Getting Started with OpenShift.go
@@ -1,3 +1,20 @@
 Code samples & snippets coming soon!
+This is a Work in Progress!
 
-// Setting up the Kubernetes.
+package main
+
+import (
+	"os"
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	ip := os.Getenv("OPENSHIFT_INTERNAL_IP")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(fmt.Sprintf("%s: 8443", ip), nil)
+}
